b328f67482427efd6848d42fc9383003fe967d96
mahmud <shahratin@gmail.com>
2018-02-21 22:01:35 +0600

    package-lock added

--- prescription.js
+++ prescription.js
@@ -1,3 +1,19 @@
 var express = require ('express');
 var bodyParser = require('body-parser');
 
+var path = require('path');
+
+app = express();
+app.use(function(req, res, next){
+	console.log("Logging");
+	next();
+})
+app.get('/' , function(req, res){
+	res.send('Mello');
+})
+
+
+app.listen(3000, function(){
+	console.log('Server started on Port	3000...');
+})
+
